e0d51e0d570462967f1837958f434002db8c8c78
Antoine DARCHE <=>
2021-06-08 19:12:59 +0200

    Change title

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,7 +4,7 @@ require(`dotenv`).config({
 
 module.exports = {
   siteMetadata: {
-    siteTitleAlt: `Jodie - Gatsby Starter Portfolio`,
+    siteTitleAlt: `Blacksheep spirit`,
   },
   plugins: [
     {
