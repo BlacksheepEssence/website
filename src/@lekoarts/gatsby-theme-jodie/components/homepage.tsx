ea3e5869f57ace8983ef45546509f1123ccc7a71
Antoine DARCHE <=>
2021-06-20 15:46:58 +0200

    Remove grid classes

--- src/@lekoarts/gatsby-theme-jodie/components/homepage.tsx
+++ src/@lekoarts/gatsby-theme-jodie/components/homepage.tsx
@@ -4,7 +4,6 @@ import React from "react";
 import Layout from "@lekoarts/gatsby-theme-jodie/src/components/layout";
 import {visuallyHidden} from "@lekoarts/gatsby-theme-jodie/src/styles/utils";
 import locales from "@lekoarts/gatsby-theme-jodie/src/locales/index";
-import {itemListWrapperStyles, itemStyles} from "@lekoarts/gatsby-theme-jodie/src/styles/item-list";
 import "../../../components/style.css"
 import photo from "/static/istockphoto-480045080-612x612.jpg"
 
@@ -21,8 +20,8 @@ const Homepage = () => {
             <h1 sx={visuallyHidden} data-testid="page-title">
                 {locales.home}
             </h1>
-            <div className={`item-list-wrapper`} sx={itemListWrapperStyles}>
-                <div className={`home-article-link item-list div${divisor}`}>
+            <div>
+                <div className="home-article-link">
                     <img src="https://cap.img.pmdstatic.net/fit/http.3A.2F.2Fprd2-bone-image.2Es3-website-eu-west-1.2Eamazonaws.2Ecom.2Fcap.2F2020.2F06.2F13.2F61a8e269-52ba-4ac4-9607-c6c9e11b73f5.2Ejpeg/790x395/background-color/ffffff/quality/90/framboises-melons-tomates-quels-sont-les-fruits-et-legumes-made-in-france-bien-plus-chers-1372584.jpg" />
                 </div>
                 <div>
