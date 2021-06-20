5e2665caebd8d69c9633cc9f9102894920a4f289
haniju <hannequin.j@gmail.com>
2021-06-20 23:32:30 +0200

    barre de nav

--- src/@lekoarts/gatsby-theme-jodie/icons/logo.tsx
+++ src/@lekoarts/gatsby-theme-jodie/icons/logo.tsx
@@ -1,10 +1,10 @@
-import logo_noir from "../../../static/logo_noir.png"
+import logo_noir from "../../../static/banner.jpg"
 
 import * as React from "react"
 
 const Logo = () => (
     <div>
-            <img alt="Pouet" src="logo_noir" width="100%" />
+      <img alt="Pouet" src="logo_noir" width="100%" />
     </div>
 )
 
