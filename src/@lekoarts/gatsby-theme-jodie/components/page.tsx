be11d8a07b569263ca9b7bf917dd45c829f7e128
Antoine DARCHE <=>
2021-07-08 19:11:02 +0200

    Add contact form

--- src/@lekoarts/gatsby-theme-jodie/components/page.tsx
+++ src/@lekoarts/gatsby-theme-jodie/components/page.tsx
@@ -6,42 +6,44 @@ import { MDXRenderer } from "gatsby-plugin-mdx"
 import Layout from "./layout"
 import SEO from "@lekoarts/gatsby-theme-jodie/src/components/seo"
 import "../../../components/style.css"
+import ContactForm from "../../../pages/contact-form.js"
 
 type DataProps = {
-  page: {
-    title: string
-    slug: string
-    excerpt: string
-    body: string
-    color: string
-    custom: boolean
-    cover: {
-      childImageSharp: {
-        resize: {
-          src: string
+    page: {
+        title: string
+        slug: string
+        excerpt: string
+        body: string
+        color: string
+        custom: boolean
+        cover: {
+            childImageSharp: {
+                resize: {
+                    src: string
+                }
+            }
         }
-      }
     }
-  }
 }
 
 const Page: React.FC<PageProps<DataProps>> = ({ data: { page }, location }) => (
-  <Layout color={page.color || undefined}>
-    <SEO
-      title={page.title}
-      description={page.excerpt}
-      pathname={location.pathname}
-      image={page.cover.childImageSharp.resize.src}
-    />
-    <div
-      sx={{
-        variant: page.custom ? `content.custom` : `content.page`,
-      }}
-      data-testid="page-content"
-    >
-      <MDXRenderer>{page.body}</MDXRenderer>
-    </div>
-  </Layout>
+    <Layout color={page.color || undefined}>
+        <SEO
+            title={page.title}
+            description={page.excerpt}
+            pathname={location.pathname}
+            image={page.cover.childImageSharp.resize.src}
+        />
+        <div
+            sx={{
+                variant: page.custom ? `content.custom` : `content.page`,
+            }}
+            data-testid="page-content"
+        >
+            <MDXRenderer>{page.body}</MDXRenderer>
+            { page.title == 'Contact' ? <ContactForm></ContactForm> : '' }
+        </div>
+    </Layout>
 )
 
 export default Page
---
+++ src/pages/contact-form.js
@@ -0,0 +1,59 @@
+import React, { useState }  from "react"
+import axios from "axios";
+const ContactForm = () => {
+
+    const [setServerState] = useState({
+        submitting: false,
+        status: null
+    });
+    const handleServerResponse = (ok, msg, form) => {
+        setServerState({
+            submitting: false,
+            status: { ok, msg }
+        });
+        if (ok) {
+            alert('Votre message a bien été envoyé.');
+            form.reset();
+        }
+    };
+    const handleOnSubmit = e => {
+        e.preventDefault();
+        const form = e.target;
+        setServerState({ submitting: true });
+        axios({
+            method: "post",
+            url: "https://getform.io/f/37164295-ec22-4226-968e-e38bffff9df3",
+            data: new FormData(form)
+        })
+            .then(r => {
+                handleServerResponse(true, "Thanks!", form);
+            })
+            .catch(r => {
+                handleServerResponse(false, r.response.data.error, form);
+            });
+    };
+    return (
+        <div className="col-md-8 mt-5">
+            <form onSubmit={handleOnSubmit}>
+                <div>
+                    <label htmlFor="email">E-mail</label>
+                    <br />
+                    <input type="email" name="email" placeholder="" />
+                </div>
+                <div>
+                    <label htmlFor="name">Nom Prénom</label>
+                    <br />
+                    <input type="text" name="name" placeholder="" />
+                </div>
+                <div>
+                    <label htmlFor="message">Message</label>
+                    <br />
+                    <textarea name="message" placeholder="Message" cols="26" rows="10"></textarea>
+                </div>
+                <button type="submit">Send</button>
+            </form>
+        </div>
+    );
+};
+
+export default ContactForm;
